40ad7f90cd63f4fe29a41866131d13028f5a3a84
George van Rensburg <georgevanr@gmail.com>
2024-08-21 19:46:22 +0200

    Update examples to have a common README. Refactor track_temp to use some common example functions. Add a README for track_temp example.

---
+++ examples/track_temp/processors.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+
+	"github.com/teamjorge/ibt"
+	"github.com/teamjorge/ibt/headers"
+	"github.com/teamjorge/ibt/utilities"
+	"golang.org/x/exp/maps"
+)
+
+// TrackTempProcessors tracks the track temperature for each lap of the ibt file
+type trackTempProcessor struct {
+	tempMap map[int]float32
+}
+
+// NewTrackTempProcessor creates and initialises a new trackTempProcessor
+func newTrackTempProcessor() *trackTempProcessor {
+	t := new(trackTempProcessor)
+
+	t.tempMap = make(map[int]float32)
+
+	return t
+}
+
+// Display name of the processor
+func (t *trackTempProcessor) Name() string { return "Track Temp" }
+
+// Method used for processing every tick of telemetry
+func (t *trackTempProcessor) Process(input ibt.Tick, hasNext bool, session *headers.Session) error {
+	trackTemp, err := ibt.GetVariableValue[float32](input, "TrackTempCrew")
+	if err != nil {
+		return err
+	}
+
+	lap, err := ibt.GetVariableValue[int](input, "Lap")
+	if err != nil {
+		return err
+	}
+
+	t.tempMap[lap] = trackTemp
+
+	return nil
+}
+
+// Utility function for create a result that can be joined with other processors.
+//
+// This will convert the results to map[int]interface{}, where the keys will refer to laps.
+// Result is not yet required by any interfaces, but is useful when using multiple processors
+// that summarise telemetry based by lap.
+func (t *trackTempProcessor) Result() map[int]interface{} {
+	return utilities.CreateGenericMap(t.tempMap)
+}
+
+// Columns required for the processor
+func (t *trackTempProcessor) Whitelist() []string { return []string{"Lap", "TrackTempCrew"} }
+
+// Print the summarised Track Temperature
+func (t *trackTempProcessor) Print() {
+	fmt.Println("Track Temp:")
+	laps := maps.Keys(t.tempMap)
+	sort.Ints(laps)
+
+	for _, lap := range laps {
+		fmt.Printf("%03d - %.3f\n", lap, t.tempMap[lap])
+	}
+}
